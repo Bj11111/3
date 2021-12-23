8bd1562509278f28a9fee69c0fda7e658db82c46
Bj11111 <95857589+Bj11111@users.noreply.github.com>
2021-12-23 20:09:57 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,34 +1,35 @@
 # Tic Tac Toe
+pip install GitPython
 
-import streamlit as st
+import random
 
 
 # 列印方法
 def drawBoard(board):
-    #這個函數打印出它被傳遞的板。
-    # “board”是一個由 10 個字符串組成的列表，代表董事會（忽略索引 0）
-    st.write('   |   |')
-    st.write(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    st.write('   |   |')
-    st.write('-----------')
-    st.write('   |   |')
-    st.write(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    st.write('   |   |')
-    st.write('-----------')
-    st.write('   |   |')
-    st.write(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    st.write('   |   |')
+    # This function prints out the board that it was passed.
+    # "board" is a list of 10 strings representing the board (ignore index 0)
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
 
 
 def inputPlayerLetter():
-    # 讓玩家輸入他們想要的字母。
-    #返回一個列表，其中玩家的字母作為第一項，計算機的字母作為第二項。
+    # Lets the player type which letter they want to be.
+    # Returns a list with the player’s letter as the first item, and the computer's letter as the second.
     letter = ''
     while not (letter == 'X' or letter == 'O'):
         print('Do you want to be X or O?')
         letter = input().upper()
 
-    # 列表中的第一個元素是玩家的字母，第二個元素是計算機的字母。
+    # the first element in the list is the player’s letter, the second is the computer's letter.
     if letter == 'X':
         return ['X', 'O']
     else:
@@ -36,7 +37,7 @@ def inputPlayerLetter():
 
 
 def whoGoesFirst():
-    # 隨機選擇先行的玩家。
+    # Randomly choose the player who goes first.
     if random.randint(0, 1) == 0:
         return 'computer'
     else:
@@ -44,8 +45,8 @@ def whoGoesFirst():
 
 
 def playAgain():
-    # 如果玩家想再次玩，此函數返回 True，否則返回 False。
-    st.write('Do you want to play again? (yes or no)')
+    # This function returns True if the player wants to play again, otherwise it returns False.
+    print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
 
@@ -56,8 +57,8 @@ def makeMove(board, letter, move):
 
 # 判斷遊戲是否結束
 def isWinner(bo, le):
-    # 給定一個棋盤和一個玩家的信，如果該玩家贏了，這個函數將返回 True。
-    # 我們使用 bo 代替 board 和 le 代替 letter，所以我們不必輸入那麼多。
+    # Given a board and a player’s letter, this function returns True if that player has won.
+    # We use bo instead of board and le instead of letter so we don’t have to type as much.
     return ((bo[7] == le and bo[8] == le and bo[9] == le) or  # across the top
             (bo[4] == le and bo[5] == le and bo[6] == le) or  # across the middle
             (bo[1] == le and bo[2] == le and bo[3] == le) or  # across the bottom
@@ -65,11 +66,11 @@ def isWinner(bo, le):
             (bo[8] == le and bo[5] == le and bo[2] == le) or  # down the middle
             (bo[9] == le and bo[6] == le and bo[3] == le) or  # down the right side
             (bo[7] == le and bo[5] == le and bo[3] == le) or  # diagonal
-            (bo[9] == le and bo[5] == le and bo[1] == le))    # diagonal
+            (bo[9] == le and bo[5] == le and bo[1] == le))  # diagonal
 
 
 def getBoardCopy(board):
-    # 複製板列表並將其返回。
+    # Make a duplicate of the board list and return it the duplicate.
     dupeBoard = []
 
     for i in board:
@@ -80,13 +81,13 @@ def getBoardCopy(board):
 
 # 驗證輸入的list值是否為空
 def isSpaceFree(board, move):
-    #如果通過的棋盤在通過的棋盤上是空的，則返回 true。
+    # Return true if the passed move is free on the passed board.
     return board[move] == ' '
 
 
 # 返回下子位置
 def getPlayerMove(board):
-    # 讓玩家輸入他們的動作。
+    # Let the player type in their move.
     move = ' '
     while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
         print('What is your next move? (1-9)')
@@ -96,8 +97,8 @@ def getPlayerMove(board):
 
 # 從這些列表裡面隨機下
 def chooseRandomMoveFromList(board, movesList):
-    # 從傳遞板上的傳遞列表中返回一個有效的移動。
-    # 如果沒有有效的移動，則返回 None。
+    # Returns a valid move from the passed list on the passed board.
+    # Returns None if there is no valid move.
     possibleMoves = []
     # 獲取空子位置list
     for i in movesList:
@@ -112,7 +113,7 @@ def chooseRandomMoveFromList(board, movesList):
 
 # 電腦獲取下子位置
 def getComputerMove(board, computerLetter):
-    #給定一塊板和計算機的字母，確定移動的位置並返回該移動。
+    # Given a board and the computer's letter, determine where to move and return that move.
     if computerLetter == 'X':
         playerLetter = 'O'
     else:
@@ -157,14 +158,14 @@ def getComputerMove(board, computerLetter):
 
 
 def isBoardFull(board):
-    # 如果板上的每個空間都已被佔用，則返回 True。否則返回 False。
+    # Return True if every space on the board has been taken. Otherwise return False.
     for i in range(1, 10):
         if isSpaceFree(board, i):
             return False
     return True
 
 
-st.write('Welcome to Tic Tac Toe!')
+print('Welcome to Tic Tac Toe!')
 
 # 死迴圈，沒有
 while True:
@@ -176,7 +177,7 @@ while True:
     # 隨機產生誰先下
     turn = whoGoesFirst()
     # 列印是誰先下
-    st.write('The ' + turn + ' will go first.')
+    print('The ' + turn + ' will go first.')
     # 遊戲開始
     gameIsPlaying = True
     while gameIsPlaying:
@@ -192,14 +193,14 @@ while True:
             # 判斷遊戲是否結束
             if isWinner(theBoard, playerLetter):
                 drawBoard(theBoard)
-                st.write('Hooray! You have won the game!')
+                print('Hooray! You have won the game!')
                 # 結束遊戲
                 gameIsPlaying = False
             else:
                 # 驗證畫板是否畫滿
                 if isBoardFull(theBoard):
                     drawBoard(theBoard)
-                    st.write('The game is a tie!')
+                    print('The game is a tie!')
                     break
                 else:
                     turn = 'computer'
@@ -212,12 +213,12 @@ while True:
 
             if isWinner(theBoard, computerLetter):
                 drawBoard(theBoard)
-                st.write('The computer has beaten you! You lose.')
+                print('The computer has beaten you! You lose.')
                 gameIsPlaying = False
             else:
                 if isBoardFull(theBoard):
                     drawBoard(theBoard)
-                    st.write('The game is a tie!')
+                    print('The game is a tie!')
                     break
                 else:
                     turn = 'player'
